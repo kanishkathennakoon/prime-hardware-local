90b00f8bccc9bde9c3bf60536dded3713c52c8f7
kanishkathennakoon <kanishka8880@gmail.com>
2026-07-24 19:18:33 +0100

    Fix product price bubbel width

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -43,7 +43,7 @@ const ProductDetailsPage = async (props: {
               <div className='flex flex-col sm:flex-row sm:items-center gap-3'>
                 <ProductPrice
                   value={Number(product.price)}
-                  className='w-24 rounded-full bg-green-100 text-green-700 px-5 py-2'
+                  className='w-fit whitespace-nowrap inline-flex items-center justify-center rounded-full bg-green-100 text-green-700 px-4 py-1.5 text-base font-semibold'
                 />
               </div>
             </div>
